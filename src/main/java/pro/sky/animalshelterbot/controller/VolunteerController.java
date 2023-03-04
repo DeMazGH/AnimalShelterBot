04a377e84905f0a0dead7b1aec6cf4cfa8694467
Denis <demaz@bk.ru>
2023-03-04 18:48:29 +0700

    - additions and changes in the documentation

--- src/main/java/pro/sky/animalshelterbot/controller/VolunteerController.java
+++ src/main/java/pro/sky/animalshelterbot/controller/VolunteerController.java
@@ -1,5 +1,11 @@
 package pro.sky.animalshelterbot.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pro.sky.animalshelterbot.model.Volunteer;
@@ -19,6 +25,29 @@ public class VolunteerController {
         this.volunteerService = volunteerService;
     }
 
+    @Operation(
+            summary = "Создание волонтера",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Данные создаваемого волонтера." +
+                            "id переданный в теле будет игнорироваться, будет присвоен следующий id из БД. " +
+                            "Все поля кроме id обязательны.",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Volunteer.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Данные созданного волонтера",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Volunteer.class)
+                            )
+
+                    )
+            }
+    )
     @PostMapping
     public ResponseEntity<Volunteer> createVolunteer(@RequestBody Volunteer volunteer) {
 
@@ -26,8 +55,27 @@ public class VolunteerController {
         return ResponseEntity.ok(createdVolunteer);
     }
 
+    @Operation(
+            summary = "Поиск волонтера по id.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Информация о найденном волонтере",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Volunteer.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Волонтер с данным id не найден"
+                    )
+            }
+    )
     @GetMapping("{volunteerId}")
-    public ResponseEntity<Volunteer> findVolunteer(@PathVariable long volunteerId) {
+    public ResponseEntity<Volunteer> findVolunteer(
+            @Parameter(description = "Идентификатор волонтера", example = "1")
+            @PathVariable long volunteerId) {
 
         Volunteer volunteer = volunteerService.findVolunteerById(volunteerId);
         if (volunteer == null) {
@@ -36,6 +84,30 @@ public class VolunteerController {
         return ResponseEntity.ok(volunteer);
     }
 
+    @Operation(
+            summary = "Изменение данных волонтера.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Измененные данные о волонтере. Все поля обязательны.",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Volunteer.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Измененные данные о волонтере",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Volunteer.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Волонтер с данным id не найден"
+                    )
+            }
+    )
     @PutMapping
     public ResponseEntity<Volunteer> updateVolunteer(@RequestBody Volunteer volunteer) {
 
@@ -46,8 +118,19 @@ public class VolunteerController {
         return ResponseEntity.ok(updatedVolunteer);
     }
 
+    @Operation(
+            summary = "Удаление волонтера по id.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Волонтер успешно удален"
+                    )
+            }
+    )
     @DeleteMapping("{volunteerId}")
-    public ResponseEntity<Volunteer> deleteVolunteer(@PathVariable long volunteerId) {
+    public ResponseEntity<Volunteer> deleteVolunteer(
+            @Parameter(description = "id удаляемого волонтера")
+            @PathVariable long volunteerId) {
 
         volunteerService.deleteVolunteer(volunteerId);
         return ResponseEntity.ok().build();
