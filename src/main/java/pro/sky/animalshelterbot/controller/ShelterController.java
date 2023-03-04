6393d8ec87b4e164f9ade8d04e107295d5089d1d
Denis <demaz@bk.ru>
2023-03-04 13:36:52 +0700

    - API documentation added

--- src/main/java/pro/sky/animalshelterbot/controller/ShelterController.java
+++ src/main/java/pro/sky/animalshelterbot/controller/ShelterController.java
@@ -1,5 +1,11 @@
 package pro.sky.animalshelterbot.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pro.sky.animalshelterbot.model.Shelter;
@@ -20,14 +26,37 @@ public class ShelterController {
     }
 
     /**
-     * Метод принимает запрос с телом в формате JSON и создает сущность {@link Shelter}
+     * Метод принимает запрос с телом в формате JSON и создает сущность {@link Shelter}.
      * Используется метод createShelter из сервиса {@link ShelterService}
      *
      * @param shelter тело запроса с данными о создаваемом приюте
      * @return данные созданного приюта
      */
+    @Operation(
+            summary = "Создание приюта",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Данные создаваемого приюта. " +
+                            "id переданный в теле будет игнорироваться, будет присвоен следующий id из БД. " +
+                            "Поля имя и адрес обязательны к заполнению.",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Shelter.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Данные созданного приюта",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Shelter.class)
+                            )
+                    )
+            }
+    )
     @PostMapping
     public ResponseEntity<Shelter> createShelter(@RequestBody Shelter shelter) {
+
         Shelter createdShelter = shelterService.createShelter(shelter);
         return ResponseEntity.ok(createdShelter);
     }
@@ -38,8 +67,30 @@ public class ShelterController {
      * @param shelterId - идентификатор приюта
      * @return 200 - данные о приюте, 404 - приют не найден
      */
+    @Operation(
+            summary = "Поиск приюта по идентификатору",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Данные о приюте",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                            schema = @Schema(implementation = Shelter.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Приют с таким id не найден"
+                    )
+            }
+    )
     @GetMapping({"{shelterId}"})
-    public ResponseEntity<Shelter> getShelter(@PathVariable long shelterId) {
+    public ResponseEntity<Shelter> getShelter(
+            @Parameter(description = "id приюта который нужно найти", example = "3")
+            @PathVariable long shelterId) {
+
         Shelter shelter = shelterService.findShelterById(shelterId);
         if (shelter == null) {
             return ResponseEntity.notFound().build();
@@ -54,8 +105,34 @@ public class ShelterController {
      * @param shelter тело запроса с измененными данными о приюте
      * @return 200 - данные измененного приюта, 404 - приют не найден
      */
+    @Operation(
+            summary = "Изменить данные о приюте",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Актуальные данные о приюте. " +
+                            "Обязательны поля: id приюта в котором нужно внести изменения, название и адрес.",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Shelter.class)
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Измененные данные о приюте",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Shelter.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Приют с таким id не найден"
+                    )
+            }
+    )
     @PutMapping
     public ResponseEntity<Shelter> updateShelter(@RequestBody Shelter shelter) {
+
         Shelter updatedShelter = shelterService.updateShelter(shelter);
         if (updatedShelter == null) {
             return ResponseEntity.notFound().build();
@@ -70,8 +147,24 @@ public class ShelterController {
      * @param shelterId идентификатор удаляемого приюта
      * @return 200
      */
+    @Operation(
+            summary = "Удаление приюта",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Приют успешно удален"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Приют с таким id не найден"
+                    )
+            }
+    )
     @DeleteMapping("{shelterId}")
-    public ResponseEntity<Shelter> deleteShelter(@PathVariable long shelterId) {
+    public ResponseEntity<Shelter> deleteShelter(
+            @Parameter(description = "id приюта который нужно удалить")
+            @PathVariable long shelterId) {
+
         shelterService.deleteShelter(shelterId);
         return ResponseEntity.ok().build();
     }
