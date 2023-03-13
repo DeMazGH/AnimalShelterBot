1bd623de28389628d48bb5e462a1e195f4fd87ca
Denis <demaz@bk.ru>
2023-03-13 12:25:42 +0700

    - add test class for VolunteerService

---
+++ src/test/java/pro/sky/animalshelterbot/service/VolunteerServiceTest.java
@@ -0,0 +1,26 @@
+package pro.sky.animalshelterbot.service;
+
+import org.junit.jupiter.api.Test;
+
+class VolunteerServiceTest {
+
+    @Test
+    void createVolunteer() {
+    }
+
+    @Test
+    void findVolunteerById() {
+    }
+
+    @Test
+    void updateVolunteer() {
+    }
+
+    @Test
+    void deleteVolunteer() {
+    }
+
+    @Test
+    void findAllVolunteers() {
+    }
+}
